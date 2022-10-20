1e22234d1c9322aa1051ff8cec8cb5aa9052b15c
SanKon228 <78688113+SanKon228@users.noreply.github.com>
2022-10-20 12:11:54 +0300

    Add files via upload

---
+++ Dz3_1.java
@@ -0,0 +1,27 @@
+class Dz3_1{  
+    public static void main (String [] args) {
+        System.out.print("1.2+31 = ");
+        double a = 1.2+31.0;
+        System.out.printf("%2f  \n", a);
+
+        System.out.print("45*54-11 = ");
+        double a1 = 45*54-11;
+        System.out.printf("%2f  \n", a1);
+
+        System.out.print("15 / 4 = ");
+        double a2 = 15 / 4;
+        System.out.printf("%2f  \n", a2);
+
+        System.out.print("15 / 4 = ");
+        double a3 = 15.0 / 4.0;
+        System.out.printf("%2f  \n", a3);
+     
+        System.out.print("67 % 5 =");
+        double a4 = 67 % 5 ;
+        System.out.printf("%2f  \n", a4);
+
+        System.out.print("67 % 5 =");
+        double a5 = 67 % 5 ;
+        System.out.printf("%2f  \n", a5);
+    }
+}
---
+++ Dz3_6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Dz3_6{  
+    public static void main (String [] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a: ");
+        String a = in.nextLine();
+        System.out.print("Input b: ");
+        String b = in.nextLine();
+        System.out.printf("!!! %s , %s  !!!\n", a,b);
+        in.close();
+    }
+    }
