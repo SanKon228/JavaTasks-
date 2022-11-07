5b27aa509962129075ecab9a97d68918e18af41e
SanKon228 <78688113+SanKon228@users.noreply.github.com>
2022-11-07 20:51:45 +0300

    Add files via upload

---
+++ T3_1_1.java
@@ -0,0 +1,8 @@
+public class T3_1_1 {
+    public static void main(String[] args) {
+        String str1 = new String(new char[] {'h', 'e', 'l', 'l', 'o'});
+    for (int i = 0; i < str1.length(); i++){
+        System.out.printf("%s \n",str1.charAt(i));
+    }
+    }
+}
